3980eee0621d57f6a28763cf061df09403bbeb1f
franklin rodriguez <franklinserif@gmail.com>
2024-09-26 15:58:48 -0400

    feat(components): add PrimaryLink

---
+++ src/app/components/PrimaryLink.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import Link from "next/link";
+import { cn } from "@libs/utils";
+
+interface Props {
+  href: string;
+  children: React.ReactNode;
+  secondary?: boolean;
+  className?: string;
+}
+
+export const PrimaryLink: FC<Props> = ({
+  href,
+  children,
+  secondary = false,
+  className = "",
+}) => {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "box-border text-secondary font-sans cursor-pointer hover:text-secondary/90 transition-colors text-nowrap",
+        className,
+        {
+          "px-[50px] py-[7px] rounded-full border-[1px] border-secondary hover:bg-secondary/50":
+            secondary,
+        }
+      )}
+    >
+      {children}
+    </Link>
+  );
+};
