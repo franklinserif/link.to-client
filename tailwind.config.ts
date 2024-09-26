323990ef09c165a08110f43e44b61886ae18e0a9
franklin rodriguez <franklinserif@gmail.com>
2024-09-26 15:58:10 -0400

    chore(tailwind.config.ts): add custom styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
       colors: {
+        main: "#172755",
+        mainDarked: "#0c1736",
+        secondary: "#8794BA",
         background: "var(--background)",
         foreground: "var(--foreground)",
+        darkBlue: "#172755",
+        pinkRed: "#EF2A82",
+        lightBlue: "#0088F0",
+        chartLines: "#EEEEF6",
       },
     },
   },
