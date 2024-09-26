f0850f67b78b493caec5337ba675191dc32a4b08
franklin rodriguez <franklinserif@gmail.com>
2024-09-26 15:59:24 -0400

    chore(constants): add MAIN_MENU

---
+++ src/app/constants/navbar.ts
@@ -0,0 +1,27 @@
+export const MAIN_MENU = [
+  {
+    id: 1,
+    href: "/product",
+    text: "Product",
+  },
+  {
+    id: 2,
+    href: "/pricing",
+    text: "Pricing",
+  },
+  {
+    id: 3,
+    href: "/faq",
+    text: "FAQ",
+  },
+  {
+    id: 4,
+    href: "/about-us",
+    text: "About us",
+  },
+  {
+    id: 5,
+    href: "/contact-us",
+    text: "Contact us",
+  },
+];
