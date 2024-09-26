89deb9438548dc57117c7152ab06d887d54da736
franklin rodriguez <franklinserif@gmail.com>
2024-09-26 15:59:56 -0400

    feat(components): add Navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { FC, useState } from "react";
+import Image from "next/image";
+import { cn } from "@libs/utils";
+import { Logo } from "@components/Logo";
+import { PrimaryLink } from "@components/PrimaryLink";
+import { Button } from "@components/Button";
+import { MAIN_MENU } from "@constants/navbar";
+
+export const Navbar: FC = () => {
+  const [isResponsiveMenuOpen, setIsResponsiveMenuOpen] = useState(false);
+
+  const handleResponiveMenu = () => {
+    setIsResponsiveMenuOpen((prevState) => !prevState);
+  };
+
+  return (
+    <div className="w-full flex flex-col items-center">
+      <nav className="flex items-center lg:mt-[50px] py-10 lg:py-0 w-full h-[40px] max-w-[1181px] px-5 box-border shadow-lg lg:shadow-none">
+        <ul className="flex mr-auto lg:mr-[81px] items-center">
+          <li>
+            <Logo />
+          </li>
+        </ul>
+        <ul className="hidden lg:flex gap-[36px] flex-nowrap items-center mr-auto">
+          {MAIN_MENU.map((link) => (
+            <li key={link.id} className="hidden md:block">
+              <PrimaryLink href={link.href}>{link.text}</PrimaryLink>
+            </li>
+          ))}
+        </ul>
+        <ul className="flex items-center gap-10 flex-nowrap text-xl lg:text-base">
+          <li>
+            <PrimaryLink href="/singin">Sign In</PrimaryLink>
+          </li>
+          <li className="hidden lg:block">
+            <PrimaryLink href="/signup" secondary>
+              Sign up
+            </PrimaryLink>
+          </li>
+          <li className="block lg:hidden">
+            <Button onClick={handleResponiveMenu}>
+              <Image
+                src="/menu-icon.svg"
+                width={40}
+                height={40}
+                alt="hamburger menu"
+              />
+            </Button>
+          </li>
+        </ul>
+      </nav>
+      <div
+        className={cn(
+          "absolute block lg:hidden bg-[#151d35] left-[-100%] transition-transform shadow-lg",
+          {
+            "inset-0 left-[50%]": isResponsiveMenuOpen,
+          }
+        )}
+      >
+        <ul className="flex flex-col items-end">
+          <div className="mt-[50px]"></div>
+          <li className="w-full pl-10">
+            <Button onClick={handleResponiveMenu}>
+              <Image
+                src="/x-menu.svg"
+                width={40}
+                height={40}
+                alt="hamburger menu"
+              />
+            </Button>
+          </li>
+          <div className="mt-[20px]"></div>
+          {MAIN_MENU.map((link) => (
+            <li
+              key={link.id}
+              className="py-4 px-10 w-full text-lg border-b-[1px] border-[#15234d] hover:bg-secondary/10"
+            >
+              <PrimaryLink href={link.href}>{link.text}</PrimaryLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
