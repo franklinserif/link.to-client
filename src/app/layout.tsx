5a787e5d68a8dc9dbcf464c517bf592e814952e0
franklin rodriguez <franklinserif@gmail.com>
2024-09-26 16:00:54 -0400

    refactor(main layout): change png favicon for svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.svg" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
