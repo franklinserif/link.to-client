e59b61cc4a709bd13ebc1c432f78c7da39e4e27c
franklin rodriguez <franklinserif@gmail.com>
2024-09-27 18:34:40 -0400

    refactor(Logo.tsx): remove layout and objectFit properties

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const Logo: FC = () => {
   return (
     <div className="relative h-[30px] w-[120px] md:h-[40px] md:w-[152px]">
-      <Image src="/logo.svg" alt="link-to" layout="fill" objectFit="contain" />
+      <Image src="/logo.svg" alt="link-to" fill />
     </div>
   );
 };
