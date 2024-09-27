a6cb9e70496cc10488feb6f1bb4fa4382c8274d5
franklin rodriguez <franklinserif@gmail.com>
2024-09-27 18:38:50 -0400

    refactor(home page): improve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Navbar } from "@components/Navbar";
+import { Header } from "@components/Header";
+import { Hero } from "@components/Hero";
 
 export default function Home() {
   return (
-    <div className="flex justify-center w-full ">
-      <div className="flex justify-center w-full ">
-        <Navbar />
-      </div>
+    <div className="flex justify-center flex-col w-full ">
+      <Header />
+      <Hero />
     </div>
   );
 }
