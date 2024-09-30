b5deb8fd32256fe9669613feb63f4eedd25155f0
franklin rodriguez <franklinserif@gmail.com>
2024-09-30 16:40:54 -0400

    chore(constants): add new PRICING_PLANS

---
+++ src/app/constants/pricing.ts
@@ -0,0 +1,29 @@
+export const PRICING_PLANS = [
+  {
+    id: 1,
+    title: "Starter",
+    description: "up to 3 users",
+    pricing: "29",
+    pricingDetails: "per month",
+    link: "/pricing",
+    white: false,
+  },
+  {
+    id: 2,
+    title: "Standard",
+    description: "up to 2 users",
+    pricing: "99",
+    pricingDetails: "per month",
+    link: "/pricing",
+    white: true,
+  },
+  {
+    id: 3,
+    title: "Premium",
+    description: "up to 200 users",
+    pricing: "299",
+    pricingDetails: "per month",
+    link: "/pricing",
+    white: false,
+  },
+];
