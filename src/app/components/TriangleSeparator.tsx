43bdbf2d8d3a09a77ed4bcbb6bff199597c7bbe1
franklin rodriguez <franklinserif@gmail.com>
2024-09-30 16:47:03 -0400

    refactor(TriangleSepator): add new variant

--- src/app/components/TriangleSeparator.tsx
+++ src/app/components/TriangleSeparator.tsx
@@ -1,9 +1,18 @@
 import { FC } from "react";
+import { cn } from "@libs/utils";
 
-export const TriangleSeparator: FC = () => {
+interface Props {
+  main?: boolean;
+}
+
+export const TriangleSeparator: FC<Props> = ({ main }) => {
   return (
     <div className="w-full relative overflow-hidden pt-7">
-      <div className="clip-triangle z-20 bg-white h-[200px] w-full relative"></div>
+      <div
+        className={cn("clip-triangle z-20 bg-white h-[200px] w-full relative", {
+          "bg-main": main,
+        })}
+      ></div>
       <div className="clip-triangle-background z-10 bg-pinkRed h-[200px] w-full absolute scale-110 bottom-[10px] left-0 right-0"></div>
     </div>
   );
