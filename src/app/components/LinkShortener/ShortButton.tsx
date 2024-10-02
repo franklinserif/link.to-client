a676560aaeae553ab184200d2991d12e9275de4a
franklin rodriguez <franklinserif@gmail.com>
2024-10-02 11:09:23 -0400

    refactor(ShortButton): improve implementation and add isLoading functionality

--- src/app/components/LinkShortener/ShortButton.tsx
+++ src/app/components/LinkShortener/ShortButton.tsx
@@ -1,12 +1,37 @@
-import { FC } from "react";
+import { FC, HTMLAttributes } from "react";
+import Image from "next/image";
+import { cn } from "@libs/utils";
 
-export const ShortButton: FC = () => {
+interface Props extends HTMLAttributes<HTMLButtonElement> {
+  isLoading?: boolean;
+}
+
+export const ShortButton: FC<Props> = ({ isLoading, ...props }) => {
   return (
-    <button className="relative inline-flex h-12 w-[170px] overflow-hidden rounded-r-lg p-[1px] focus:ring-2 z-10 outline-none">
-      <span className="absolute cursor-pointer inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-      <span className="inline-flex uppercase h-full w-full cursor-pointer items-center justify-center rounded-r-lg bg-pinkRed hover:bg-pinkRed/40 px-3 py-1 text-nowrap text-sm font-medium text-white backdrop-blur-3xl outline-none transition-all">
-        shorten link
-      </span>
+    <button
+      className="relative inline-flex h-12 w-[170px] overflow-hidden rounded-r-lg p-[1px] focus:ring-2 z-10 outline-none"
+      {...props}
+    >
+      <span
+        className={
+          "absolute cursor-pointer inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"
+        }
+      />
+      <p
+        className={cn(
+          "inline-flex uppercase h-full w-full cursor-pointer items-center justify-center rounded-r-lg bg-pinkRed hover:bg-pinkRed/40 px-3 py-1 text-nowrap text-sm font-medium text-white backdrop-blur-3xl outline-none transition-all",
+          { "bg-pinkRed/10": isLoading }
+        )}
+      >
+        {isLoading ? (
+          <Image src="/rolling.svg" alt="loading" width={60} height={60} />
+        ) : (
+          <>
+            <span className="hidden md:block">shorten link</span>
+            <span className="block md:hidden">short</span>
+          </>
+        )}
+      </p>
     </button>
   );
 };
