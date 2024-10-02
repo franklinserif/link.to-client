98003192df750a1c6331ea4f1420ec97f1781df5
franklin rodriguez <franklinserif@gmail.com>
2024-10-02 11:12:19 -0400

    chore(LinkShortenerSection): improvement

--- src/app/components/LinkShortener/LinkShortenerSection.tsx
+++ src/app/components/LinkShortener/LinkShortenerSection.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { FC } from "react";
-import { LinkShortenerInput } from "@components/LinkShortener/LinkShortenerInput";
+import { LinkShortenerForm } from "@components/LinkShortener/LinkShortenerForm";
 
 export const LinkShortenerSection: FC = () => {
   return (
-    <section className="mt-20 max-w-[800px] w-full h-[600px] flex justify-center items-center flex-col">
-      <h1 className="relative z-10 text-6xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-main  text-center font-sans font-bold">
+    <section className="mt-20 max-w-[900px] w-full h-[600px] flex justify-center items-center flex-col">
+      <h1 className="relative px-5 z-10 text-6xl lg:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-main  text-center font-sans font-bold">
         Paste the URL to be shortened
       </h1>
       <br />
-      <LinkShortenerInput />
+      <LinkShortenerForm />
     </section>
   );
 };
