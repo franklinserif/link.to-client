a752ab7807c866b21d8dc4d951f65b2ad495b518
franklin rodriguez <franklinserif@gmail.com>
2024-10-05 16:12:11 -0400

    refactor(HTTPLinks): improve

--- src/app/http/links.ts
+++ src/app/http/links.ts
@@ -3,8 +3,8 @@ import { Link } from "@interfaces/entities";
 export abstract class HTTPLinks {
   static host = process.env.HOST || "http://localhost:3000";
 
-  static async getLink(id: string) {
-    const response = await fetch(`${this.host}/links/information/${id}`, {
+  static async getLink(id: string): Promise<[Link | null, Error | null]> {
+    const response = await fetch(`${this.host}/api/links/${id}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -12,11 +12,39 @@ export abstract class HTTPLinks {
     });
 
     if (!response.ok) {
-      console.log("error: ", response);
+      return [null, new Error(`can't get link with id ${id}`)];
     }
 
     const linkInformation = await response.json();
 
-    return linkInformation as Link;
+    if (!linkInformation.id) {
+      return [null, new Error(`something bad happend try again`)];
+    }
+
+    return [linkInformation as Link, null];
+  }
+
+  static async createLink(
+    urlOriginal: string
+  ): Promise<[Link | null, Error | null]> {
+    const response = await fetch(`${this.host}/api/links`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ urlOriginal }),
+    });
+
+    if (!response.ok) {
+      [null, new Error(`can't create a short link`)];
+    }
+
+    const linkInformation = await response.json();
+
+    if (!linkInformation?.id) {
+      return [null, new Error(`can't create a short link try again`)];
+    }
+
+    return [linkInformation as Link, null];
   }
 }
