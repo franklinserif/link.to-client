c321b0345dc9c35e7a2ebfbc9e0fa18ed7f08e6b
franklin rodriguez <franklinserif@gmail.com>
2024-10-05 13:39:52 -0400

    chore(home page): improve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import { Header } from "@components/Header";
 import { HomePageContent } from "@components/HomePage";
 import { Footer } from "@components/Footer";
+import { LinkShortener } from "@components/LinkShortener/LinkShortener";
+import { Params } from "@interfaces/searchParams";
+import { validateSearchParams } from "@libs/params";
 
-export default function Home() {
+export default function Home({ searchParams }: { searchParams: Params }) {
+  const paramId = validateSearchParams(searchParams, "id");
+
   return (
     <div className="flex justify-center flex-col w-full ">
-      <Header />
+      <Header>
+        <LinkShortener id={paramId} />
+      </Header>
       <HomePageContent />
       <Footer />
     </div>
