f91322f19c5533cb33a6b18695e5fbe645094b0c
franklin rodriguez <franklinserif@gmail.com>
2024-10-05 16:13:00 -0400

    refactor(LinkShortenerForm): improve component

--- src/app/components/LinkShortener/LinkShortenerForm.tsx
+++ src/app/components/LinkShortener/LinkShortenerForm.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useState } from "react";
+import { useRouter } from "next/navigation";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ShortButton } from "@components/LinkShortener/ShortButton";
 import { Input } from "@components/Form/Input";
 import { ErrorMessage } from "@components/LinkShortener/ErrorMessage";
 import { linkShortenerFormSchema } from "@schemas/linkShortenerForm";
+import { HTTPLinks } from "@http/links";
 import { TLinkShortenerForm } from "@interfaces/linkShortenerForm";
 
 export const LinkShortenerForm: FC = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<any>();
+
   const {
     register,
     handleSubmit,
@@ -20,7 +26,19 @@ export const LinkShortenerForm: FC = () => {
     resolver: zodResolver(linkShortenerFormSchema),
   });
 
-  const onSubmit: SubmitHandler<TLinkShortenerForm> = (data) => {};
+  const onSubmit: SubmitHandler<TLinkShortenerForm> = async (data) => {
+    setIsLoading(true);
+    const [link, error] = await HTTPLinks.createLink(data.linkInput);
+
+    setIsLoading(false);
+    if (error) {
+      return setErrorMessage(error);
+    }
+
+    if (link) {
+      router.push(`/?id=${link.id}`);
+    }
+  };
 
   return (
     <form
@@ -36,10 +54,10 @@ export const LinkShortenerForm: FC = () => {
           placeholder="write your link e.g https://www.johndoe.com"
           className="z-10 px-5 py-3 rounded-l-full w-full max-w-[700px] text-white bg-secondary/70 shadow-inner outline-none placeholder:italic placeholder:text-main"
         />
-        <ShortButton isLoading={false} />
+        <ShortButton isLoading={isLoading} />
       </fieldset>
       <ErrorMessage
-        error={errors.linkInput}
+        error={errors.linkInput || errorMessage}
         onCleanError={() => clearErrors("linkInput")}
       />
     </form>
