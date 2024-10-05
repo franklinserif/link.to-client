384d0f2e75c45b37ab9c758b4c89d76513b8fad7
franklin rodriguez <franklinserif@gmail.com>
2024-10-05 13:38:12 -0400

    feat(LinkShortener): improve

--- src/app/components/LinkShortener/LinkShortener.tsx
+++ src/app/components/LinkShortener/LinkShortener.tsx
@@ -1,15 +1,24 @@
-"use client";
-
 import { FC } from "react";
 import { LinkShortenerForm } from "@components/LinkShortener/LinkShortenerForm";
 import { LinkShortCard } from "@components/LinkShortener/LinkShortCard";
+import { Link } from "@interfaces/entities";
+import { HTTPLinks } from "@http/links";
 
-export const LinkShortenerSection: FC = () => {
-  const linkInformation = { id: null };
+interface Props {
+  id: string | undefined;
+}
+
+export const LinkShortener: FC<Props> = async ({ id }) => {
+  let link: Link | undefined = undefined;
+
+  if (id) {
+    link = await HTTPLinks.getLink(id);
+  }
+
   return (
     <section className="mt-20 max-w-[900px] w-full h-[600px] flex justify-center items-center flex-col">
-      {linkInformation.id ? (
-        <LinkShortCard />
+      {link?.id ? (
+        <LinkShortCard link={link} />
       ) : (
         <>
           <h1 className="relative px-5 z-10 text-6xl lg:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-main  text-center font-sans font-bold">
